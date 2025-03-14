3f80354c9d09d0c09b3e73a9bd5788f3c982f950
QwikLab Explorers <155870248+Titash-shil@users.noreply.github.com>
2025-03-14 22:26:40 +0530

    Update qwiklab_explorers_gsp1125.sh

--- qwiklab_explorers_gsp1125.sh
+++ qwiklab_explorers_gsp1125.sh
@@ -1,4 +1,3 @@
-
 export ZONE=$(gcloud compute project-info describe \
 --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
 
@@ -20,6 +19,7 @@ jq '{
     } 
   ] 
 } + .' policy.json > updated_policy.json
+
 
 gcloud projects set-iam-policy $(gcloud config get-value project) updated_policy.json
 
@@ -44,8 +44,34 @@ gcloud compute instances create instance-1 \
 --metadata=enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD \
 --scopes=https://www.googleapis.com/auth/cloud-platform --create-disk=auto-delete=yes,boot=yes,device-name=instance-1,image=projects/debian-cloud/global/images/debian-11-bullseye-v20230912,mode=rw,size=10,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced
 
-gcloud dns --project=$DEVSHELL_PROJECT_ID policies create dns-test-policy --description="Qwiklab" --networks="default" --private-alternative-name-servers="" --no-enable-inbound-forwarding --enable-logging
+gcloud dns --project=$DEVSHELL_PROJECT_ID policies create dns-test-policy --description="quickgcplab" --networks="default" --private-alternative-name-servers="" --no-enable-inbound-forwarding --enable-logging
 
 sleep 30
 
 gcloud compute ssh instance-1 --zone=$ZONE --tunnel-through-iap --project "$DEVSHELL_PROJECT_ID" --quiet --command "gcloud projects get-iam-policy \$(gcloud config get project) && curl etd-malware-trigger.goog"
+
+# Function to prompt user to check their progress
+function check_progress {
+    while true; do
+        echo
+        echo -n "${BOLD}${YELLOW}Have you checked your progress for Task 1 & Task 2? (Y/N): ${RESET}"
+        read -r user_input
+        if [[ "$user_input" == "Y" || "$user_input" == "y" ]]; then
+            echo
+            echo "${BOLD}${GREEN}Great! Proceeding to the next steps...${RESET}"
+            echo
+            break
+        elif [[ "$user_input" == "N" || "$user_input" == "n" ]]; then
+            echo
+            echo "${BOLD}${RED}Please check your progress for Task 1 & Task 2 and then press Y to continue.${RESET}"
+        else
+            echo
+            echo "${BOLD}${MAGENTA}Invalid input. Please enter Y or N.${RESET}"
+        fi
+    done
+}
+
+# Call function to check progress before proceeding
+check_progress
+
+gcloud compute instances delete instance-1 --zone=$ZONE --quiet
